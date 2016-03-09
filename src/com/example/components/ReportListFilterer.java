7340f8a59407921111337ace4ce53edfcb90625b
Simo-Pekka Koskinen <Pasadenos@gmail.com>
2016-03-09 22:34:07 +0200

    Horizontally aligned assignee-selections

--- src/com/example/components/ReportListFilterer.java
+++ src/com/example/components/ReportListFilterer.java
@@ -22,6 +22,7 @@ public class ReportListFilterer extends CustomComponent {
 		
 		filtererContainer.setSizeUndefined();
 		filterOptions.setSizeUndefined();
+		filterOptions.addStyleName("horizontal");
 		
 		setCompositionRoot(filtererContainer);
 	}
