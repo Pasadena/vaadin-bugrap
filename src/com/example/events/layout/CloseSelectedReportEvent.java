11033bbf137732ff9b4f91e6d45bb56677c7685b
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-03-31 15:45:20 +0300

    Added new layout-specific event

---
+++ src/com/example/events/layout/CloseSelectedReportEvent.java
@@ -0,0 +1,13 @@
+package com.example.events.layout;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Component.Event;
+
+@SuppressWarnings("serial")
+public class CloseSelectedReportEvent extends Event {
+
+	public CloseSelectedReportEvent(Component source) {
+		super(source);
+	}
+
+}
